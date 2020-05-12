08189aec42c608955c82bdaa49ec01cf3e78d3dd
noraeisner <nora.eisner@physics.ox.ac.uk>
2020-05-12 12:41:39 +0200

    tpf example file

--- dkafhjks/data/tesscurl_sector_all_tp.sh
+++
@@ -1,1 +0,0 @@
-#all LC file links
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "READMEpip.md").read_text()
 # This call to setup() does all the work
 setup(
     name="tessLATTE",
-    version="0.1.17",
+    version="0.1.18",
     description="Lightcurve Analysis Tool for Transiting Exoplanets",
     long_description=README,
     long_description_content_type="text/markdown",
