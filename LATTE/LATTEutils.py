d9b7a2a99f64f557aec316a6dd2c1abe2d8b824e
orionlee <orionlee@yahoo.com>
2021-07-19 18:59:07 -0700

    bug fix regression on saving _aperture_size.png #39

--- LATTE/LATTEutils.py
+++ LATTE/LATTEutils.py
@@ -5156,6 +5156,8 @@ def plot_aperturesize(tic, indir, TESS_unbinned_t_l, TESS_binned_t_l, small_binn
             plt.show()
         else:
             plt.close()
+    
+    else:   
 
 
         plt.figure(figsize=(gs*6,4))
